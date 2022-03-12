e9c5566d547ac3445ab5b7af9f762ef94d50f9f3
gleitons <31367377+gleitons@users.noreply.github.com>
2022-03-12 12:40:31 -0300

    contador regressivo

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const relogio = setInterval( function time() {
     minutos.innerText = minuto
     segundos.innerText = segundo
 }/*Aqui pode definir o tempo colocando ", 3000", onde 3000 é a quantidade de milisegundos */)
+
+function starTime (duration, display){
+var timer = duration, minutes, seconds;
+setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.innerText = minutes + ":" + seconds;
+
+    if(--timer < 0) {
+        timer = duration
+    }
+
+}, 1000)
+}
+window.onload = function() {
+    var duration = 60 * 4;
+    var display = document.getElementById('timer');
+    starTime(duration, display)
+}
